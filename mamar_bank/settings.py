ec4ccd549e6d52179d4b9d7b2e2858bb9c88df2b
Sagor Ahmed <summyalsagor8722@gmail.com>
2026-01-08 12:14:27 +0600

    added

--- mamar_bank/settings.py
+++ mamar_bank/settings.py
@@ -34,7 +34,9 @@ SECRET_KEY = env("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+# ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["local-bank.onrender.com", "127.0.0.1", "localhost"]
+
 
 
 # Application definition
